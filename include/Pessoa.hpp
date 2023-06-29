eded33e3884fec3d17102acf4ab605e6e9acb640
Auanny de Oliveira <130615392+auannyos@users.noreply.github.com>
2023-06-29 20:13:32 -0300

    Create Pessoa.hpp

---
+++ include/Pessoa.hpp
@@ -0,0 +1,27 @@
+#ifndef PESSOA_HPP
+#define PESSOA_HPP
+
+#include <string>
+
+class Pessoa {
+    private:
+        std::string nome;
+        std::string email;
+        std::string senha;
+
+    public:
+        Pessoa(std::string nome, std::string email, std::string senha);
+
+        std::string getNome();
+        std::string getEmail();
+        std::string getSenha();
+
+        void setNome(std::string nome);
+        void setEmail(std::string email);
+        void setSenha(std::string senha);
+
+        bool login(std::string email, std::string senha);
+        bool logout();
+};
+
+#endif
