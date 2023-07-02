7ae7ea9a377aeb63009e26ae1bfed10d09fc3500
Vanelli2002 <130325758+Vanelli2002@users.noreply.github.com>
2023-07-02 19:28:37 -0300

    Atualização Usuario.cpp

--- src/entidades/Usuario.cpp
+++ src/entidades/Usuario.cpp
@@ -1,17 +1,12 @@
 #include "Usuario.hpp"
 
 Usuario::Usuario(std::string nome, std::string email, std::string senha,
-                std::string curso, std::string idEstudantil) {
-    this->nome = nome;
-    this->email = email;
-    this->senha = senha;
+                std::string curso, std::string idEstudantil)
+    : Pessoa(nome, email, senha) {
     this->curso = curso;
     this->idEstudantil = idEstudantil;
 }
 
-std::string Usuario::getNome() {
-    return this->nome;
-}
 
 std::string Usuario::getCurso() {
     return this->curso;
@@ -19,10 +14,6 @@ std::string Usuario::getCurso() {
 
 std::string Usuario::getIdEstudantil() {
     return this->idEstudantil;
-}
-
-std::vector<Livro> Usuario::getLivros() {
-    return this->livros;
 }
 
 int Usuario::getDiasAtraso() {
@@ -37,10 +28,6 @@ std::string Usuario::getLinkPagamento() {
     return this->linkPagamento;
 }
 
-void Usuario::setNome(std::string nome) {
-    this->nome = nome;
-}
-
 void Usuario::setCurso(std::string curso) {
     this->curso = curso;
 }
@@ -49,14 +36,18 @@ void Usuario::setIdEstudantil(std::string idEstudantil) {
     this->idEstudantil = idEstudantil;
 }
 
-void Usuario::setLivros(std::vector<Livro> livros) {
-    this->livros = livros;
-}
-
 void Usuario::setDiasAtraso(int diasAtraso) {
     this->diasAtraso = diasAtraso;
 }
 
 void Usuario::setMulta(float multa) {
     this->multa = multa;
+}
+
+void Usuario::pegarLivroEmprestado(Livro livro, Data dataEmprestimo){
+    this->emprestimo.adicionarLivro(livro, dataEmprestimo);
+}
+
+std::vector<std::map<Livro, std::map<Data, Data>>> Usuario::getLivrosEmprestados(){
+    return this->emprestimo.getLivros();
 }
