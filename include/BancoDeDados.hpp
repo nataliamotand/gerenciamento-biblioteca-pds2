35342f7dfd1e01cbbf93cc05e6db3344255d9b5b
vitoriapbraga <131930526+vitoriapbraga@users.noreply.github.com>
2023-07-03 08:50:42 -0300

    Update BancoDeDados.hpp

--- include/BancoDeDados.hpp
+++ include/BancoDeDados.hpp
@@ -2,22 +2,35 @@
 #define BANCODEDADOS_HPP
 
 #include "Usuario.hpp"
+#include "Administrador.hpp"
 
 #include <vector>
+#include <string>
+#include <iostream>
 
 
 class BancoDeDados {
     private:
-        std::vector<Usuario> usuarios;
+        std::vector<Usuario*> usuarios;
+
+
 
     public:
         BancoDeDados();
 
-        std::vector<Usuario> getUsuarios();
+        std::vector<Usuario*> getUsuarios();
 
-        bool adicionarUsuario(Usuario usuario);
-        bool atualizarUsuario(Usuario usuario);
-        bool removerUsuario(Usuario usuario);
+        bool adicionarUsuario(std::string nome, std::string email, std::string senha,
+                std::string curso, std::string idEstudantil);
+        bool atualizarUsuario(std::string nome, std::string email, std::string senha,
+                std::string curso, std::string idEstudantil);
+        bool removerUsuario(std::string email);
+        void imprimeTodosUsuarios();
+
+        bool loginAdministrador(std::string email, std::string senha);
+
+
+        ~BancoDeDados();
 };
 
 #endif
