9d9fb459e518510ac220f1880b6950dd8a00c77f
Paulo Victor <pvivanelli@gmail.com>
2023-07-04 09:08:30 -0300

    arquivos data inicio

---
+++ include/Data.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <string>
+#include <iostream>
+
+class Data{
+    private:
+        int dia;
+        int mes;
+        int ano;
+    public:
+};
+#endif
