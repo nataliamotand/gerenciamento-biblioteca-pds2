e8f767eb272668d239c5fa85922180bbc6387e90
Paulo Victor <pvivanelli@gmail.com>
2023-07-04 09:09:25 -0300

    arquivo hpp data public

--- include/Data.hpp
+++ include/Data.hpp
@@ -10,5 +10,14 @@ class Data{
         int mes;
         int ano;
     public:
+        Data();
+        Data(int dia, int mes, int ano);
+        Data getData() const;
+        int getDia();
+        int getMes();
+        int getAno();
+        void setDia(int dia);
+        void setMes(int mes);
+        void setAno(int ano);
 };
 #endif
