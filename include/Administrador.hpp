8cbda3c4507f7dc86886f0a14dfe6cd70b8853c5
nmota <nataliamotand@gmail.com>
2023-07-05 15:17:24 -0300

    fix: alteracoes administrador

--- include/Administrador.hpp
+++ include/Administrador.hpp
@@ -1,34 +1,21 @@
+
+
 #ifndef ADMINISTRADOR_HPP
 #define ADMINISTRADOR_HPP
 
 // #include "Livro.hpp"
+#include "Pessoa.hpp"
 #include "Usuario.hpp"
 // #include "Emprestimo.hpp"
-#include "BancoDeDados.hpp"
 
 #include <string>
 
-class Administrador {
+class Administrador : public Pessoa {
     private:
-        std::string nome = "Biblioteca";
-        std::string email = "biblioteca@ufmg.br";
-        std::string senha = "1234";
+
 
     public:
-        std::string getNome();
-        std::string getSenha();
-        std::string getEmail();
-
-        void setNome(std::string nome);
-        void setSenha(std::string senha);
-        void setEmail(std::string email);
-
-        bool login(std::string email, std::string senha);
-        bool logout();
-    
-        void cadastrarUsuario(std::string nome, std::string email,
-                              std::string senha, std::string curso,
-                              std::string idEstudantil);
+        Administrador();
 };
 
 #endif
