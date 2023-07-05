22ed674d2a2000c05c4163351c55231aeda21776
Auanny de Oliveira <130615392+auannyos@users.noreply.github.com>
2023-07-05 10:43:53 -0300

    feat: header file para Emprestimo

---
+++ include/Emprestimo.hpp
@@ -0,0 +1,29 @@
+#ifndef EMPRESTIMO_HPP
+#define EMPRESTIMO_HPP
+
+#include "Livro.hpp"
+
+#include <string>
+#include <vector>
+#include <map>
+
+class Emprestimo {
+    private:
+        // <Livro, Data de empréstimo, Data de devolução>
+        std::vector<std::map<Livro, std::map<Data, Data>>> livros;
+        // dia(s) atrasado(s) e livro(s) atrasado(s)
+        std::vector<std::map<Livro*, int>> diasAtrasados;
+        // multa(s) e livro(s) atrasado(s)
+        std::vector<std::map<Livro*, float>> multa;
+        const int diasPadraoEmprestimo = 14;
+
+    public:
+        Emprestimo();
+        std::vector<std::map<Livro, std::map<Data, Data>>> getLivros();
+        int getDiasPadraoEmprestimo();
+        Data calcularDataDevolucao(Data dataEmprestimo);
+        void adicionarLivro(Livro livro, Data dataEmprestimo);
+};
+
+
+#endif
