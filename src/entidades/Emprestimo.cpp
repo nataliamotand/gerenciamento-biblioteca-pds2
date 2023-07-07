b1d057479560b17b4db640bf992429e143c950de
Auanny de Oliveira <130615392+auannyos@users.noreply.github.com>
2023-07-06 21:01:36 -0300

    atualização Emprestimo.cpp

--- src/entidades/Emprestimo.cpp
+++ src/entidades/Emprestimo.cpp
@@ -13,6 +13,10 @@ int Emprestimo::getDiasPadraoEmprestimo(){
     return this->diasPadraoEmprestimo;
 }
 
+int Emprestimo::getMaximoLivrosEmprestados(){
+    return this->maximoLivrosEmprestados;
+}
+
 Data Emprestimo::calcularDataDevolucao(Data dataEmprestimo){
     Data dataDevolucao = dataDevolucao.adicionarDias(dataEmprestimo, this->diasPadraoEmprestimo);
     return dataDevolucao;
@@ -20,4 +24,20 @@ Data Emprestimo::calcularDataDevolucao(Data dataEmprestimo){
 
 std::vector<std::map<Livro, std::map<Data, Data>>> Emprestimo::getLivros(){
     return this->livros;
+}
+
+bool Emprestimo::renovarLivro(std::string titulo, Data dataAtual){
+    for (std::vector<std::map<Livro, std::map<Data, Data>>>::size_type i = 0; i < this->livros.size(); i++){
+        for (auto it = this->livros[i].begin(); it != this->livros[i].end(); it++){
+            if (it->first.getTitulo() == titulo){
+                it->second.begin()->second = calcularDataDevolucao(dataAtual);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Emprestimo::podePegarLivroEmprestado(){
+    return static_cast<int>(this->livros.size()) < this->maximoLivrosEmprestados;
 }
